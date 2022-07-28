f1336ffc00ddc9068fd03c895b4bd5e0b1dfe78b
Parker <prmac@Parkers-MacBook-Pro.local>
2022-07-27 22:49:52 -0600

    Added comments

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//variables for the query selectors
 var time = document.querySelector(".time");
 var heading = document.querySelector("header");
 var pageContent = document.querySelector(".content");
@@ -6,6 +7,7 @@ var subScore = document.querySelector("#subScore");
 var userId = document.querySelector("#user");
 var scoresBtn = document.querySelector(".scoresBtn");
 
+//variables to create different elements
 var quizTitle = document.createElement("h1");
 var someText = document.createElement("p");
 var startButton = document.createElement("button");
@@ -16,6 +18,7 @@ var ans4 = document.createElement("button");
 var result = document.createElement("p");
 var submitMess = document.createElement("h5");
 
+//normal variables used throughout the code
 var timeLeft = 60;
 var score = 0;
 var page = 0;
@@ -25,17 +28,19 @@ var resets = 0;
 var finalUser = [];
 var finalScore= [];
 
+//add classes to the answers
 ans1.classList.add("btn");
 ans2.classList.add("btn");
 ans3.classList.add("btn");
 ans4.classList.add("btn");
 
-
+//setting attributes and event listeners
 heading.setAttribute("style", "display: flex; justify-content: space-between;");
 pageContent.setAttribute("style", "text-align: center;")
 scoresBtn.addEventListener('click', highscores);
 pageContent.classList.add("pageC");
 
+//Starts the time on the quiz when the button is pressed
 function startTime() {
 
     var countdown = setInterval(function() {
@@ -56,20 +61,24 @@ function startTime() {
     }, 1000);
 }
 
+//This is the first page that shows up on the page
 function mainPage(){
 
+    //removes and adds the event listener if the user decides to play again
     startButton.removeEventListener('click', reset);
     startButton.setAttribute("onclick", "nextPage()");
 
+    //content for the main page
     quizTitle.textContent = "Coding Quiz";
     someText.textContent = "Get ready for the most intense quiz ever";
     startButton.textContent = "Start Quiz";
 
-    // pageContent.appendChild(userId);
+    //appends the elements to page
     pageContent.appendChild(quizTitle);
     quizTitle.appendChild(someText);
     pageContent.appendChild(startButton);
 
+    //only the first time playing through does this occur
     if (resets == 0) {
         userId.remove();
     }
@@ -77,27 +86,31 @@ function mainPage(){
 
 }
 
+//Function for the next page in the quiz
 function nextPage() {
 
+    //Starts the timer in the top right and makes the page number 1
     startTime();
     page = 1;
 
+    //Changes the content on the for the first question
     quizTitle.textContent = "";
     someText.textContent = "What type of data stores a value of true of false?";
     ans1.textContent = "String";
     ans2.textContent = "Number";
     ans3.textContent = "Boolean";
 
-    
-
+    //Removes the start button from the screen
     startButton.remove();
 
+    //Shows the answers on the screen
     pageContent.appendChild(someText);
     options.style.display = "block";
     options.appendChild(ans1);
     options.appendChild(ans2);
     options.appendChild(ans3);
 
+    //Only runs when the user wants to replay
     if (resets >= 1) {
     someText.appendChild(ans1);
     someText.appendChild(ans2);
@@ -106,6 +119,7 @@ function nextPage() {
         
     }
 
+    //Removes and resets the event listners for multiple games
     ans1.removeEventListener("click", decreaseTime);
     ans2.removeEventListener("click", decreaseTime);
     ans3.removeEventListener("click", decreaseTime);
@@ -115,12 +129,9 @@ function nextPage() {
     ans2.addEventListener("click", decreaseTime);
     ans3.addEventListener("click", increaseScore);
 
-
-    console.log(score);
-    console.log(page);
-
 }
 
+//Function to increase the score and change the page if the user gets the answer right
 function increaseScore() {
     score += 20;
     console.log(score);
@@ -140,6 +151,7 @@ function increaseScore() {
     return;
 }
 
+//Function to decrease the time and change the page if the user's answer is wrong
 function decreaseTime() {
     if (timeLeft > 0) {
     timeLeft = timeLeft - 10;
@@ -160,6 +172,7 @@ function decreaseTime() {
     return;
 }
 
+//Third page and second question. The rest of the quiz questions are set up the same as the first question
 function thirdPage() {
 
     page = 2;
@@ -292,17 +305,18 @@ function sixthPage() {
     console.log(page);
 }
 
+//Function for the last page
 function allDone() {
     
     page = 6;
 
+    //Text for final page
     someText.textContent = "All Done!";
-
     options.style.display = "block";
-
     result.textContent = `You scored ${score}/100`;
-
     subScore.style.display = "inline";
+
+    //Appends text depending if the user is playing for the first time or multiple times
     if (resets == 0) {
         options.appendChild(result);
         options.appendChild(userId);
@@ -312,9 +326,6 @@ function allDone() {
     if (resets >= 1) {
         
         result.style.display = "block";
-        // options.appendChild(result);
-        // options.appendChild(userId);
-        // options.appendChild(subScore);
 
         someText.appendChild(options);
         options.appendChild(result);
@@ -323,11 +334,13 @@ function allDone() {
         userId.appendChild(subScore);
         }
 
+    //removes the answers from the page
     ans1.remove();
     ans2.remove();
     ans3.remove();
     ans4.remove();
 
+    //Creates a field for the user to input their initials
     subScore.addEventListener("click", function(event) {
         event.preventDefault();
     
@@ -353,6 +366,7 @@ function allDone() {
 
 }
 
+//Stores the user's initials in local storage
 function store(itemId) {
     users.push(itemId);
     localStorage.setItem("User1", JSON.stringify(users));
@@ -360,12 +374,14 @@ function store(itemId) {
     
 }
 
+//Stores the user's score in local storage
 function scoreStore(itemId) {
     scores.push(itemId);
     localStorage.setItem("Score", JSON.stringify(scores));
     finalScore = JSON.parse(localStorage.getItem("Score"))
 }
 
+//Lists the score of the users
 function scoreList() {
     var orderList = document.createElement("ol");
     pageContent.appendChild(orderList);
@@ -375,26 +391,20 @@ function scoreList() {
     }
 }
 
-
+//The highscore page
 function highscores() {
     quizTitle.textContent = "Highscores";
-    // result.textContent = score + "% - " + localStorage.getItem("User");
 
     var orderList = document.createElement("ul");
     var listItem = document.createElement("li");
     
-
     pageContent.appendChild(quizTitle);
-    // pageContent.appendChild(result);
     pageContent.appendChild(startButton);
-    // scoreList();
-
     quizTitle.appendChild(orderList);
 
+    //Displays the sores to the page
     for (i = 0; i < finalUser.length; i++) {
         var itemNumber = i + 1;
-        // listItem.textContent = itemNumber + ".  " + finalScore[i] + " - " + finalUser[i];
-        // orderList.appendChild(listItem);
         orderList.appendChild(document.createElement("li"));
 
         if (page > 0) {
@@ -404,18 +414,17 @@ function highscores() {
         orderList.children[i].textContent = itemNumber + ".  " + finalScore[i] + " - " + finalUser[i];
     }
 
-
+    //Play again button
     startButton.textContent = "Play Again?";
-
     startButton.removeAttribute("onclick", "nextPage()");
     startButton.addEventListener('click', reset);
 
     someText.remove();
     options.style.display = "none";
-    // console.log(localStorage.getItem("User"));
 
 }
 
+//Resets everything for the next time the user wants to play and runs the game again
 function reset() {
     score = 0;
     page = 0;
@@ -427,6 +436,5 @@ function reset() {
     mainPage();
 }
 
-
-
+//Runs the game
 mainPage();
